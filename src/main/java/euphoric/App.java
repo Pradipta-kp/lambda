5a905d588cdb3ff8843c44c23429f9500fcf99f3
pradipta-kp <pradipta.patra@euphoricthought.com>
2019-01-24 13:20:45 +0000

    addin maven project

---
+++ src/main/java/euphoric/App.java
@@ -0,0 +1,13 @@
+package euphoric;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
